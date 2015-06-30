b9f007d1c61b10ef712e9eba9a69b5b3038bcfc5
gollivier <gollivier@avignon.inra.fr>
2015-06-30 15:53:38 +0200

    ee

---
+++ issuecrawlR.R
@@ -0,0 +1,25 @@
+# crawl successif représentés par tranches ou sous forme de gexf dynamique
+# deal with xml export files
+# xml contains data on the crawl, startingpoints, date
+
+https://www.issuecrawler.net/files/inm_318734.xml
+
+
+# successive crawls
+
+xml2igraph <- function(id) {
+  
+  url= 
+    
+    
+return(g)
+}
+
+
+get_successive_graph <- function(list_crawl_id, gexf=T) { 
+
+
+
+  
+
+}
